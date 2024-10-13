e630d49b66a67d076374f65c34f373730ce30fa7
Braian Rios <braian.rios@ing.austral.edu.ar>
2024-10-13 00:27:47 -0300

    building docker image with envs

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,14 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
-      'process.env': env
+      'process.env': {
+        VITE_FRONTEND_URL: env.VITE_FRONTEND_URL,
+        VITE_BACKEND_URL: env.VITE_BACKEND_URL,
+        VITE_RUNNER_URL: env.VITE_RUNNER_URL,
+        VITE_AUTH0_DOMAIN: env.VITE_AUTH0_DOMAIN,
+        VITE_AUTH0_AUDIENCE: env.VITE_AUTH0_AUDIENCE,
+        VITE_AUTH0_CLIENT_ID: env.VITE_AUTH0_CLIENT_ID,
+      }
     },
     plugins: [react()],
   }
